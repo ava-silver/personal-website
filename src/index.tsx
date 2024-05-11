6e17a3bc207290e1216b27142df6c4fb1433736c
Ava Silver <avapsilver@gmail.com>
2024-05-11 14:55:59 -0400

    mothers day page 2024

--- src/index.tsx
+++ src/index.tsx
@@ -1,4 +1,19 @@
 import { createRoot } from 'react-dom/client';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import App from './App';
+import MothersDay from './MothersDay';
 
-createRoot(document.getElementById('root')!).render(<App />);
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <App />,
+  },
+  {
+    path: '/mom',
+    element: <MothersDay />,
+  },
+]);
+
+createRoot(document.getElementById('root')!).render(
+  <RouterProvider router={router} />
+);
