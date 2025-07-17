08f6a463ef24c1e5b3e3997be843c5969dadcb2a
Ava Silver <avapsilver@gmail.com>
2025-07-17 00:25:23 -0400

    simplify

--- rss.sh
+++ rss.sh
@@ -17,20 +17,15 @@ EOF
 ls src/Blog/posts/*.md | while read -r file; do
     filename=$(basename "$file" .md)
     htmlfile="./build/rss/${filename}.html"
-
     pandoc "$file" -f markdown -t html -o $htmlfile
-    
-    post_date=$(cut -d'-' -f1-3 <<<"$filename")
-    post_date=$(date -d "$post_date" +"%d %b %Y 00:00:00 %z")
-    post_title=$(grep '# ' $file | cut -d'#' -f2 | xargs)
-    post_id=$(cut -d'-' -f4- <<<"$filename")
-    post_link="https://avasilver.dev/blog/${post_id}"
+
+    post_link="https://avasilver.dev/blog/$(cut -d'-' -f4- <<<"$filename")"
     cat >> public/feed.xml <<EOF
     <item>
-      <title>${post_title}</title>
+      <title>$(grep '# ' $file | cut -d'#' -f2 | xargs)</title>
       <link>${post_link}</link>
       <guid isPermaLink="true">${post_link}</guid>
-      <pubDate>${post_date}</pubDate>
+      <pubDate>$(cut -d'-' -f1-3 <<<"$filename" | xargs -I{} date -d {} +"%d %b %Y 00:00:00 %z")</pubDate>
       <description><![CDATA[$(cat $htmlfile)]]></description>
     </item>
 EOF
